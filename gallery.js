00aa5152c48c5009d2b40a4a7f7d075c38ff2353
Matthew <mpachapa@uncc.edu>
2022-03-10 14:41:57 -0500

    go away notifications

--- gallery.js
+++ gallery.js
@@ -10,25 +10,6 @@ window.onload = function() {
 		document.getElementById("btn7").onclick = changeImage7;
         document.getElementById("btn8").onclick = changeImage8;
         document.getElementById("btn9").onclick = changeImage9;
-		document.getElementById("btn10").onclick = changeImage10;
-        document.getElementById("btn11").onclick = changeImage11;
-        document.getElementById("btn12").onclick = changeImage12;
-		document.getElementById("btn13").onclick = changeImage13;
-        document.getElementById("btn14").onclick = changeImage14;
-        document.getElementById("btn15").onclick = changeImage15;
-		document.getElementById("btn16").onclick = changeImage16;
-        document.getElementById("btn17").onclick = changeImage17;
-        document.getElementById("btn18").onclick = changeImage18;
-		document.getElementById("btn19").onclick = changeImage19;
-        document.getElementById("btn20").onclick = changeImage20;
-        document.getElementById("btn21").onclick = changeImage21;
-		document.getElementById("btn22").onclick = changeImage22;
-        document.getElementById("btn23").onclick = changeImage23;
-        document.getElementById("btn24").onclick = changeImage24;
-		document.getElementById("btn25").onclick = changeImage25;
-		document.getElementById("btn26").onclick = changeImage26;
-        document.getElementById("btn27").onclick = changeImage27;
-        document.getElementById("btn28").onclick = changeImage28;
 };
 
 
@@ -62,60 +43,3 @@ function changeImage9() {
 function changeImage10() {
 	document.getElementById("myImage").src="Slides/slide10.png";      
 }
-function changeImage11() {
-	document.getElementById("myImage").src="Slides/slide11.png";      
-}
-function changeImage12() {
-	document.getElementById("myImage").src="Slides/slide12.png";      
-}
-function changeImage13() {
-	document.getElementById("myImage").src="Slides/slide13.png";      
-}
-function changeImage14() {
-	document.getElementById("myImage").src="Slides/slide14.png";
-}
-function changeImage15() {
-	document.getElementById("myImage").src="Slides/slide15.png";
-}
-function changeImage16() {
-	document.getElementById("myImage").src="Slides/slide16.png";      
-}
-function changeImage17() {
-	document.getElementById("myImage").src="Slides/slide17.png";      
-}
-function changeImage18() {
-	document.getElementById("myImage").src="Slides/slide18.png";      
-}
-function changeImage19() {
-	document.getElementById("myImage").src="Slides/slide19.png";      
-}
-function changeImage20() {
-	document.getElementById("myImage").src="Slides/slide20.png";      
-}
-function changeImage21() {
-	document.getElementById("myImage").src="Slides/slide21.png";      
-}
-function changeImage22() {
-	document.getElementById("myImage").src="Slides/slide22.png";      
-}
-function changeImage23() {
-	document.getElementById("myImage").src="Slides/slide23.png";      
-}
-function changeImage24() {
-	document.getElementById("myImage").src="Slides/slide24.png";      
-}
-function changeImage25() {
-	document.getElementById("myImage").src="Slides/slide25.png";      
-}
-function changeImage26() {
-	document.getElementById("myImage").src="Slides/slide26.png";      
-}
-function changeImage27() {
-	document.getElementById("myImage").src="Slides/slide27.png";      
-}
-function changeImage28() {
-	document.getElementById("myImage").src="Slides/slide28.png";      
-}
-
-
-
